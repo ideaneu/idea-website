0f7b33e3265bd3cb1023c94de7a721267706fb72
Drew <drewpom@icloud.com>
2016-09-28 19:48:49 -0400

    remove old idea theme

--- wp-content/themes/idea-theme/template-contact.php
+++ wp-content/themes/idea-theme/template-contact.php
@@ -6,7 +6,7 @@
  * @package Scout-Base
  */
 // This adds the contactForm.css to the ninjaforms main styling within the plugin so we can customize the form style
-// First it tests to see if there is a form on the page with the ID of 5 and then if it does it adds the styles. 
+// First it tests to see if there is a form on the page with the ID of 5 and then if it does it adds the styles.
 function contactForm_add_nf_styles( $form_id ) {
   if( $form_id == 5 ) {
     wp_enqueue_style('contactForm-nf-css' , get_template_directory_uri() . '/stylesheets/contactForm.css', '3.0.1' );
@@ -31,14 +31,14 @@ add_action ( 'ninja_forms_display_css', 'contactForm_add_nf_styles' );
       <div class="container">
         <div class="row">
           <div class="col-md-6 col-md-offset-3">
-            <p>If you’re interested in getting started at IDEA with your concept, the first step is to attend New Venture Orientation. 
+            <p>If you’re interested in getting started at IDEA with your concept, the first step is to attend New Venture Orientation.
               At this session you will learn about the IDEA program, available resources, and next steps for developing your venture. </p>
               <div class="text-center"><a href="http://www.tfaforms.com/365595" target="_blank"><button id="contact-startVenture__button" class="btn btn-lg"> Start a Venture</button></a></div>
             </div>
           </div>
         </div>
       </section>
-      
+
       <hr/>
       <section id="contact-form">
         <h2 class="centered">Contact Us</h2>
@@ -60,7 +60,7 @@ add_action ( 'ninja_forms_display_css', 'contactForm_add_nf_styles' );
           <h2 class="centered">Visiting Idea</h2>
           <div class="col-md-5">
             <p>
-              The IDEA lab is located in the Hayden Hall basement which is 370 
+              The IDEA lab is located in the Hayden Hall basement which is 370
               Huntington Avenue, Boston MA 02115.
             </p>
             <p>
@@ -70,10 +70,10 @@ add_action ( 'ninja_forms_display_css', 'contactForm_add_nf_styles' );
               Boston, MA 02116
             </p>
             <p>
-              <strong>Parking: </strong>There is parking available in the Renaissance Parking 
+              <strong>Parking: </strong>There is parking available in the Renaissance Parking
               Garage at <a href="https://www.google.com/maps/place/835+Columbus+Ave,+Northeastern+University,+Boston,+MA+02120/@42.3361566,-71.0880542,15z/data=!4m2!3m1!1s0x89e37a22fe33d8eb:0xd8e99c7caa3bd5e9" target="_blank">835 Columbus Ave</a>.
               <br><br>
-              <strong>Public Transportation: </strong>Take the orange line to Ruggle Station or the green line to Northeastern.
+              <strong>Public Transportation: </strong>Take the orange line to Ruggles Station or the green line to Northeastern.
             </p>
           </div>
           <div class="col-md-6 col-lg-5 col-lg-offset-2 col-md-offset-1">
