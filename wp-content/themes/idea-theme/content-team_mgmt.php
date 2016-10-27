4a46378dabe4429bcae2fe2c8162f0227276d4ec
Drew <drew@pomerleaudesign.com>
2016-10-26 21:06:52 -0400

    new footer, email fix

--- wp-content/themes/idea-theme/content-team_mgmt.php
+++ wp-content/themes/idea-theme/content-team_mgmt.php
@@ -10,9 +10,9 @@
                 <!-- This one whole faculty / staff thumbnail that appears on the archive page -->
                 <!-- This code is generated multiple times by wordpress -->
 <div class="col-xs-6 col-md-4 col-lg-3 <?php $terms = get_the_terms( $post->ID, 'team_type'); if (! empty($terms)) : foreach ( $terms as $term ) {echo $term->slug.' '; } endif;  ?>">
-	<div class="team-member"> 
+	<div class="team-member">
 		<div class="box">
-	    <img id="team-thumb" src="<?php the_field('team_profile_picture'); ?>" class="img-responsive">        
+	    <img id="team-thumb" src="<?php the_field('team_profile_picture'); ?>" class="img-responsive">
 	    <div class="row team__caption">
 	        <div class="col-xs-12 team__name"><?php the_title(); ?><br /></div>
 
@@ -23,9 +23,9 @@
 	                    <img class="team__img" src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/icons/team-linkedin2.png">
 	                </a>
 	            <?php endif; ?>
-	            
+
 	            <?php if(get_field('team_email')): ?>
-	                <a href='mailto:"<?php the_field('team_email');?>"' class="email_icons">
+	                <a href='mailto:<?php the_field('team_email');?>' class="email_icons">
 	                    <img class="team__img" src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/icons/team-email.png">
 	                </a>
 	            <?php endif; ?>
--- wp-content/themes/idea-theme/footer.php
+++ wp-content/themes/idea-theme/footer.php
@@ -12,63 +12,42 @@
 
 <div id="colophon" class="site-footer" role="contentinfo">
 	<div class="container">
-		<div class="row">
-			
-			<!-- COLUMN 3 -->
-			<div class="col-sm-4 col-lg-push-9 col-sm-push-8 footer-add-pad">
-				
-				<!-- Button trigger modal -->
-				<button class="btn btn-primary btn-lg site-footer-button" data-toggle="modal" data-target="#myModal">
-					SIGN UP NOW
-				</button>
-
-				<!-- Modal -->
-				<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-					<div class="modal-dialog">
-						<div class="modal-content">
-							<div class="modal-header">
-								<button type="button" class="close modal-close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>			      </div>
-								<iframe class="modal-iframe" src="http://eepurl.com/bsBPvf"></iframe>
-								<div class="modal-body">
-								</div>
-							</div>
-						</div>
-					</div>
-					<p>Join our mailing list!</p>
+		<?php dynamic_sidebar( 'footer-1' ); ?>
+		<div class="row site-footer-bottom-row">
+				<div class="col-sm-4">
+					<img src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/logos/northeastern-logo.png" height="100%" width="100%">
 				</div>
-
-				<!-- COLUMN 2 -->				
-				<div class="col-sm-4 col-lg-push-1 footer-add-pad">
-					<div class="footer-get-involved">
-						<a href="http://www.tfaforms.com/365595" target="_blank"><button class="btn btn-lg site-footer-button" type="button">start a venture</button></a>
-						<p>Are you working on a concept or early-stage venture that you want to enroll with IDEA? Click here to RSVP to New Venture Orientation.</p>
-					</div>
-				</div>
-				<!-- COLUMN 1 -->
-				<div class="col-sm-4 col-sm-pull-8 footer-add-pad">
+				<div class="col-sm-offset-4 col-sm-4">
 					<div class="footer-social-media"><p>
-						<a href="https://www.facebook.com/IDEA.NEU" target="_blank"><img src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/icons/facebook.png"></a>
-						<a href="https://www.twitter.com/IDEANEU" target="_blank"><img src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/icons/twitter.png"></a>
-						<a href="https://www.linkedin.com/company/idea-northeastern's-venture-accelerator" target="_blank"><img src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/icons/linkedin.png"></a>
-						<strong><a href="mailto:idea@neu.edu" class="footer-email"> | idea@neu.edu</a></strong></p>
+						<a class="footer-social-media-icon" href="https://www.facebook.com/IDEA.NEU" target="_blank"><i class="fa fa-facebook"></i></a>
+						<a class="footer-social-media-icon" href="https://www.twitter.com/IDEANEU" target="_blank"><i class="fa fa-twitter"></i></a>
+						<a class="footer-social-media-icon" href="https://www.linkedin.com/company/idea-northeastern's-venture-accelerator" target="_blank"><i class="fa fa-linkedin"></i></a>
+						<span class="footer-social-media-seperator"></span>
+						<a href="mailto:idea@neu.edu">idea@neu.edu</a></p>
 					</div>
-					<div class="footer-neu-logo">
-						<img src="<?php bloginfo('url'); ?>/wp-content/themes/idea-theme/images/logos/northeastern-logo.png" height="100%" width="100%">
-					</div>
-				</div> 
+				</div>
 			</div><!-- .row -->
 		</div><!-- .container -->
+		<div class="footer-scout">
+			<p>Built by <a href="http://neu.edu/scout">Scout</a> © 2015</p>
 		</div>
-			<footer class="footer-scoutBanner">
-				<p>Built by <a href="http://neu.edu/scout" id="footer-scoutBanner--logo">Scout</a> © 2015</p>
-	</footer><!-- #colophon -->
+		</div>
 </div><!-- #page -->
 
-
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close modal-close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>			      </div>
+				<iframe class="modal-iframe" src="http://eepurl.com/bsBPvf"></iframe>
+				<div class="modal-body">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
-
-
-
 </body>
 </html>
