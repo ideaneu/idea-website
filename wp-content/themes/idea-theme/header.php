982840e2cb8dbf44a33b8962d7320c498de43f8a
Drew <drew@pomerleaudesign.com>
2017-02-13 10:29:17 -0500

    new header styles

--- wp-content/themes/idea-theme/header.php
+++ wp-content/themes/idea-theme/header.php
@@ -1,4 +1,61 @@
 <?php
+class CSS_Menu_Maker_Walker extends Walker {
+
+  var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
+
+  function start_lvl( &$output, $depth = 0, $args = array() ) {
+    $output .= '<ul class="dropdown-menu"><div class="sub-menu-intro-text"><h3>Resources</h3><p>Text here<p></div><div class="sub-menu-links">';
+  }
+
+  function end_lvl( &$output, $depth = 0, $args = array() ) {
+    if (depth == 0) {
+      $output .= '</div>';
+    }
+    $output .= "</ul>";
+  }
+
+  function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+    global $wp_query;
+    $indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+    $class_names = $value = '';
+    $classes = empty( $item->classes ) ? array() : (array) $item->classes;
+
+    /* Check for children */
+    $children = get_posts(array('post_type' => 'nav_menu_item', 'nopaging' => true, 'numberposts' => 1, 'meta_key' => '_menu_item_menu_item_parent', 'meta_value' => $item->ID));
+    if (!empty($children)) {
+      $classes[] = 'dropdown';
+    }
+
+    $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+    $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+    $id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+    $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+    $attributes = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) .'"' : '';
+    $attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) .'"' : '';
+    $attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) .'"' : '';
+    $attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) .'"' : '';
+    if (!empty($children)) {
+      $attributes .= ' class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"';
+    }
+    $item_output = '<li ' . $class_names . '>';
+    $item_output .= $args->before;
+    $item_output .= '<a ' . $attributes . '>';
+    $item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+    $item_output .= '</a>';
+    $item_output .= $args->after;
+    // $item_output = '</li>';
+
+    $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+  }
+
+  function end_el( &$output, $item, $depth = 0, $args = array() ) {
+  $output .= "</li>";
+  }
+}
+
+
 /**
  * The header for our theme.
  *
@@ -29,23 +86,28 @@
   <div id="page" class="hfeed site">
     <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'scout-base' ); ?></a>
 
-    <header id="masthead" class="site-header" role="banner">
+    <nav class="navbar">
+      <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img class="nav-logo" src="<?php bloginfo('template_directory'); ?>/images/logos/IDEA_logo_full.png" width="200px" height="100px">
+          </a>
+        </div>
 
-      <nav id="site-navigation" class="main-navigation" role="navigation">
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
-      <a href="<?php bloginfo('template_directory'); ?>/home">
-          <img class="nav-logo" src="<?php bloginfo('template_directory'); ?>/images/logos/IDEA_logo_full.png" width="200px" height="100px">
-      </a>
+          <?php wp_nav_menu( array( 'menu_class' => 'nav navbar-nav navbar-right', 'theme_location' => 'primary', 'walker' => new CSS_Menu_Maker_Walker() ) ); ?>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
 
-        <button class="menu-toggle"><i class="fa fa-bars fa-2x"></i>
-          <?php _e( '', 'scout-base' ); ?></button>
-        <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-      </nav><!-- #site-navigation -->
-
-      <?php if ( is_front_page() && (get_field('show_notification', 'options') === true ) ) { ?>
-        <?php get_template_part( 'content', 'header_notification' ); ?>
-      <?php } ?>
-
-    </header><!-- #masthead -->
 
     <div id="content" class="site-content">
